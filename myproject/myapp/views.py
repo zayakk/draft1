bba7e782321d28ed5382a18a2f053da4948d1ba2
zayakk <zaya.batsuren@gmail.com>
2024-09-18 12:37:01 +0800

    time server

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
+
+    
+def current_time(request):
+    # now = timezone.now()
+    return HttpResponse(datetime.now())
+
